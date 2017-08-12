0ef8bb8f92a41c0ba840aa5751d3b2724973eb4c
Radek Em <guitarfreak@test.com.pl>
2017-08-12 23:48:15 +0200

    Initial commit

---
+++ src/main/java/io/github/guit4rfre4k/designpatterns/MainClass.java
@@ -0,0 +1,13 @@
+package io.github.guit4rfre4k.designpatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
