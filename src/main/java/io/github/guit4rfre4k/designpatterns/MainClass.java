a286652548878a4a768999643e92513eca8ee677
Radoslaw Muzdzak <radoslaw.muzdzak@ericsson.com>
2018-06-10 20:24:52 +0200

    change java to 1.8 + sample comment
    Signed-off-by: Radoslaw Muzdzak <radoslaw.muzdzak@ericsson.com>

--- src/main/java/io/github/guit4rfre4k/designpatterns/MainClass.java
+++ src/main/java/io/github/guit4rfre4k/designpatterns/MainClass.java
@@ -2,12 +2,12 @@ package io.github.guit4rfre4k.designpatterns;
 
 /**
  * Hello world!
+ * Sample comment
  *
  */
-public class MainClass 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
