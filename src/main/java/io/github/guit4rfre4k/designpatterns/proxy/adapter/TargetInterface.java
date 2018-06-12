c45b1a7080977ac62a564cee17c10d7d56802ee6
Radoslaw Muzdzak <radoslaw.muzdzak@ericsson.com>
2018-06-12 14:43:15 +0200

    Init implemtation of the Adapter DP (training)

---
+++ src/main/java/io/github/guit4rfre4k/designpatterns/proxy/adapter/TargetInterface.java
@@ -0,0 +1,15 @@
+package io.github.guit4rfre4k.designpatterns.proxy.adapter;
+
+/**
+ *
+ * @author ezmuzra
+ */
+public interface TargetInterface {
+
+    public void targetMethod1();
+
+    public void targetMethod2();
+
+    public void targetMethod3();
+
+}
