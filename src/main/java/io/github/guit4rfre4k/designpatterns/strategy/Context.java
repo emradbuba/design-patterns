80bcfdc6124cf2af8140e0473f25f630d683e453
Radoslaw Muzdzak <radoslaw.muzdzak@ericsson.com>
2018-06-13 11:14:57 +0200

    Sample implementation of Strategy DP - training

---
+++ src/main/java/io/github/guit4rfre4k/designpatterns/strategy/Context.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.guit4rfre4k.designpatterns.strategy;
+
+/**
+ *
+ * @author ezmuzra
+ */
+public class Context {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        DocumentGeneratoingStrategy strategy;
+        //
+        strategy = new PDFStrategy();
+        client.setGeneratingStrategy(strategy);
+        client.generateDocument();
+        //
+        strategy = new DocStrategy();
+        client.setGeneratingStrategy(strategy);
+        client.generateDocument();
+    }
+
+}
