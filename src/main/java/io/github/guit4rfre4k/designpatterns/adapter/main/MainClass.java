b87df630fcee6f4ebafbfcdd3a68e176acfd5171
emrad <dontmailme@fake.com>
2019-03-03 20:32:16 +0100

    Add the second phase of the adapter design pattern demo

--- src/main/java/io/github/guit4rfre4k/designpatterns/adapter/main/MainClass.java
+++ src/main/java/io/github/guit4rfre4k/designpatterns/adapter/main/MainClass.java
@@ -1,5 +1,7 @@
 package io.github.guit4rfre4k.designpatterns.adapter.main;
 
+import io.github.guit4rfre4k.designpatterns.adapter.system_a.PersonRecordA;
+import io.github.guit4rfre4k.designpatterns.adapter.system_b.PersonFromSystemAAdapter;
 import io.github.guit4rfre4k.designpatterns.adapter.system_b.PersonalRecordB;
 import io.github.guit4rfre4k.designpatterns.adapter.system_b.PersonalRecordBImpl;
 import io.github.guit4rfre4k.designpatterns.adapter.system_b.PersonalRecordsRaportGenerator;
@@ -12,14 +14,11 @@ public class MainClass {
     public static void main(String[] args) {
 
         Set<PersonalRecordB> recordsB =  new HashSet<>();
+        recordsB.add(new PersonFromSystemAAdapter(new PersonRecordA("Gregor", "Matt", "Sitlinksy", (short) 29)));
         recordsB.add(new PersonalRecordBImpl("Radek Michal", "Mzdk", 1988));
         recordsB.add(new PersonalRecordBImpl("Angelo Micheal", "Schwarz", 1993));
         recordsB.add(new PersonalRecordBImpl("Martha Isabell", "Bright", 1998));
-        // complication error:
-        //recordsB.add(new PersonRecordA("Arthur", "Jack", "Lemon", 54));
-        //recordsB.add(new PersonRecordA("Gregor", "Matt", "Sitlinksy", 29));
-
-
+        recordsB.add(new PersonFromSystemAAdapter(new PersonRecordA("Arthur", "Jack", "Lemon", (short) 54)));
         PersonalRecordsRaportGenerator generator = new PersonalRecordsRaportGenerator();
         generator.printRaport(recordsB);
 
